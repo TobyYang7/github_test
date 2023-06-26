7140f8cc9fd1f72aa689fd81fb7d8b20e1da35a2
TobyYang7 <1658685923@qq.com>
2023-06-25 19:53:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,33 @@
-from icecream import ic
+# from icecream import ic
 
 
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    less_arr, equal_arr, greater_arr = [], [], []
-    for num in arr:
-        if num < pivot:
-            less_arr.append(num)
-        elif num > pivot:
-            greater_arr.append(num)
-        else:
-            equal_arr.append(num)
-    return quick_sort(less_arr) + equal_arr + quick_sort(greater_arr)
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     pivot = arr[len(arr) // 2]
+#     less_arr, equal_arr, greater_arr = [], [], []
+#     for num in arr:
+#         if num < pivot:
+#             less_arr.append(num)
+#         elif num > pivot:
+#             greater_arr.append(num)
+#         else:
+#             equal_arr.append(num)
+#     return quick_sort(less_arr) + equal_arr + quick_sort(greater_arr)
 
 
-name = "GitHub Copilot"
-age = 2
-test_val = 'test'
-is_copilot = True
+# name = "GitHub Copilot"
+# age = 2
+# test_val = 'test'
+# is_copilot = True
 
-ic(quick_sort([1, 3, 6, 2, 4]))
-ic(name, age, is_copilot)
-ic(test_val)
+# ic(quick_sort([1, 3, 6, 2, 4]))
+# ic(name, age, is_copilot)
+# ic(test_val)
 
-print('test value')
+# print('test value')
+
+# 创建一个字典
+my_dict = {"apple": 1, "banana": 2, "orange": 3}
+for i in my_dict:
+    print(my_dict[i])
