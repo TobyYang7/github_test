8edda46309d007189cee856194f8d95a2da4d235
TobyYang7 <1658685923@qq.com>
2023-06-28 08:42:13 -0700

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int *p, x; x = 3; p = &x;
-    printf("p points to %d\n", *p);
-
+    int x[5];
+    x[0] = 254;
+    x[1] = 649;
+    x[2] = 971;
+    x[3] = 1678;
+    unsigned char *y = (unsigned char *)x;
+    // we'll run some print statements here
+    printf("%d\n", *y);
     return 0;
 }
